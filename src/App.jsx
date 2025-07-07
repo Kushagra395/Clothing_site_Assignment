d7cd563a445afc854e418c0c1d419484fdcbedaf
kushagra <kushagramalviya3@gmail.com>
2025-07-07 15:15:57 +0530

    deploy needed change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProductListingPage from "./Pages/ProductListingPage";
-import { CartProvider } from "./Context/CartContext";
+import { CartProvider } from "./context/CartContext";
 
 
 const App = () => {
